0e63f932b209eb24137ea260d8d0f14babbe7a35
Pegasus <pegasus@yunio.com>
2017-10-18 19:53:49 -0700

    user can play game now

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 const dummyData = [
-  { name: 'game1', price: 60 },
-  { name: 'game2', price: 30 },
-  { name: 'game3', price: 40 },
-  { name: 'game4', price: 10 },
-  { name: 'game5', price: 50 }
+  { name: 'game1', price: 60, contentLength: 10 },
+  { name: 'game2', price: 30, contentLength: 20 },
+  { name: 'game3', price: 40, contentLength: 50 },
+  { name: 'game4', price: 10, contentLength: 15 },
+  { name: 'game5', price: 50, contentLength: 5 }
 ];
 
 class User {
   constructor() {
-    this.cash = 50;
+    this.cash = 100;
     this.monthlyIncome = 50;
+    this.games = [];
   }
 
   dailyThings(day) {
     if (day % 7 === 0) {
       this.gotMonthlyIncome();
     }
-    if (Math.random() >= 0.5) {
-      this.checkSteam();
+    if (this.games.length > 2) {
+      if (Math.random() >= 1 - 0.4/this.games.length) {
+        this.checkSteam();
+      } else {
+        this.playGame(this.games[0]);
+      }
+    } else if (this.games.length > 0) {
+      if (Math.random() >= 0.7) {
+        this.checkSteam();
+      } else {
+        this.playGame(this.games[0]);
+      }
+    } else {
+      if (Math.random() >= 0.3) {
+        this.checkSteam();
+      }
     }
+    console.log(`games left: ${this.games.length}`);
   }
 
   gotMonthlyIncome() {
@@ -47,6 +63,17 @@ class User {
   buyGame(game) {
     console.log(`user bought ${game.name} for ${game.price}$`);
     this.cash -= game.price;
+    this.games.push(game);
+  }
+
+  playGame(game) {
+    let playTime = Math.floor(Math.random() * 4) + 1;
+    console.log(`user played ${game.name} for ${playTime} hour(s)`);
+    game.contentLength = game.contentLength <= playTime ? 0 : game.contentLength - playTime;
+    if (game.contentLength === 0) {
+      console.log(`user beated ${game.name}`);
+      this.games.shift();
+    }
   }
 
 }
